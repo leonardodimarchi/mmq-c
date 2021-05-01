9645ded33d8d95fd0b9a9c4dde5c2ed30d1f3e19
leonardo <leonardodimarchi@outlook.com>
2021-05-01 18:02:32 -0300

    feat(aplicarMetodoReta): adicionando calculo e exibição dos vetores e sistema escalar

--- mmq_program.c
+++ mmq_program.c
@@ -1,4 +1,5 @@
-/* Passos do Metodo dos Minimos Quadrados
+/* 
+Passos do Metodo dos Minimos Quadrados
 
 1) Recebimento dos dados da tabela
 
@@ -8,16 +9,16 @@
   2.3) Calcular e exibir o sistema resultante da Eliminacao de Gauss
 
 3) Exibir o polinomio p(x) obtido atraves do sistema equivalente (Eliminacao de Gauss)
-
 */
 
 
-/*
+/* 
 - Vai ter só reta ou parabola
-- Retas são matrizes 2x2
-- Parabolas são Matrizes 3x3 
+- Retas são matrizes 2x3 (linha e coluna)
+- Parabolas são Matrizes 3x4 (linha e coluna)
 
 - O vetor u0 é tudo 1 
+- O vetor u1 é o proprio X
 */
 
 #include<stdio.h>
@@ -25,11 +26,18 @@
 
 void alocaInt(int **p, int tam);
 void alocaFloat(float **p, int tam);
-void qtdNumerosTabela(int *num);
+void recebeQtdNumerosTabela(int *num);
 void receberTermos(int *num, float *x, float *y);
+void aplicarMetodoReta(int *quantidadeTermos, float *y, float *x);
+void calculaMostraVetoresReta(int *quantidadeTermos, float *y, float *x, float *u0, float *u1);
+void calculaMostraSistemaEscalar(int *quantidadeTermos, float *y, float *u0, float *u1, float *sistemaMatriz);
+void mostrarMatriz(float *matriz, int quantidadeDeLinhas, int quantidadeDeColunas);
+
+//Parabola
+void calculaMostraVetoresParabola(int *quantidadeTermos, float *y, float *x, float *u0, float *u1, float *u2);
 
 int main() {
-  float *matriz=NULL, *x = NULL, *y = NULL;
+  float *x = NULL, *y = NULL;
   int *quantidadeTermos = NULL, opc;
 
   alocaInt(&quantidadeTermos, 1);
@@ -41,16 +49,18 @@ int main() {
   receberTermos(quantidadeTermos, x, y);
 
   do{
-    printf("\nEscolha: [1] Reta\t[2] Parabola\n");
+    printf("\nEscolha: [1] Reta\t[2] Parabola: ");
     scanf("%i", &opc);
 
     switch(opc){
       case 1: 
-        //fazer MMQ de reta
+        aplicarMetodoReta(quantidadeTermos, y, x);
         break;
+
       case 2:
-        //fazer MMQ de parabola
+        //aplicarMetodoParabola();
         break;
+
       default:
         printf("\nPor favor, tente novamente...\n");
     }
@@ -59,6 +69,114 @@ int main() {
 
   system("PAUSE");
   return 0;
+}
+
+void aplicarMetodoReta(int *quantidadeTermos, float *y, float *x){
+  float *sistemaMatriz=NULL,*u0=NULL, *u1=NULL;
+
+  alocaFloat(&sistemaMatriz,6);
+  alocaFloat(&u0, *quantidadeTermos);
+  alocaFloat(&u1, *quantidadeTermos);
+
+  calculaMostraVetoresReta(quantidadeTermos, y, x, u0, u1);
+  calculaMostraSistemaEscalar(quantidadeTermos, y, u0, u1, sistemaMatriz);
+}
+
+void calculaMostraSistemaEscalar(int *quantidadeTermos, float *y, float *u0, float *u1, float *sistemaMatriz){
+  int linha, coluna, contador;
+  float resultado=0;
+
+  //ponteiro+(<linha acessada>*total de colunas+<coluna acessada>)
+
+  //Realiza os calculos do sistema escalar
+  for(linha=0; linha < 2; linha++){
+
+    for(coluna=0; coluna < 3; coluna++){
+
+      *(sistemaMatriz+(linha*3+coluna)) = 0;
+
+      for(contador=0; contador < *quantidadeTermos; contador++){
+
+        //Seleciona o vetor, de acordo com a coluna
+        if(coluna == 0){
+          resultado =  *(u0+contador);
+        }else if(coluna == 1){
+          resultado =  *(u1+contador);
+        }else{
+          resultado =  *(y+contador);
+        }
+
+        //Seleciona o outro vetor de acordo com a linha
+        if(linha == 0){
+          resultado *= *(u0+contador);
+        }else{
+          resultado *= *(u1+contador);
+        }
+
+        //A posicao ira receber o resultado
+        *(sistemaMatriz+(linha*3+coluna)) += resultado;  
+      }
+
+    }
+  }
+
+  printf("\n--- Sistema Escalar ---\n");
+  mostrarMatriz(sistemaMatriz, 2, 3);
+}
+
+void mostrarMatriz(float *matriz, int quantidadeDeLinhas, int quantidadeDeColunas){
+  int linha, coluna;
+
+  for(linha=0; linha < quantidadeDeLinhas; linha++){  
+
+    for(coluna=0; coluna < quantidadeDeColunas; coluna++){    
+      printf("%.2f \t", *(matriz+linha*quantidadeDeColunas+coluna));  
+    }   
+
+    printf("\n");
+  }
+}
+
+void calculaMostraVetoresReta(int *quantidadeTermos, float *y, float *x, float *u0, float *u1){
+  int contador;
+
+  //Construindo o vetor u0
+  for(contador=0; contador < *quantidadeTermos; contador++){
+    *(u0+contador) = 1;
+  }
+
+  //Construindo o vetor u1
+  for(contador=0; contador < *quantidadeTermos; contador++){
+    *(u1+contador) = *(x+contador);
+  }
+
+  printf("\n--- Vetores ---\n");
+  printf("y\tu0\tu1\n");
+  for(contador=0; contador < *quantidadeTermos; contador++){
+    printf("%.2f\t%.2f\t%.2f\n", *(y+contador), *(u0+contador), *(u1+contador));
+  }
+}
+
+void calculaMostraVetoresParabola(int *quantidadeTermos, float *y, float *x, float *u0, float *u1, float *u2){
+  int contador;
+
+  for(contador=0; contador < *quantidadeTermos; contador++){
+    *(u0+contador) = 1;
+  }
+
+  for(contador=0; contador < *quantidadeTermos; contador++){
+    *(u1+contador) = *(x+contador);
+  }
+  
+  for(contador=0; contador < *quantidadeTermos; contador++){
+    *(u2+contador) = *(x+contador) * *(x+contador);
+  }
+
+  printf("\n--- Vetores ---\n");
+  printf("y\tu0\tu1\tu2\n");
+  for(contador=0; contador < *quantidadeTermos; contador++){
+    printf("%.2f\t%.2f\t%.2f\t%.2f\n", *(y+contador), *(u0+contador), *(u1+contador), *(u2+contador));
+  }
 }
 
 void receberTermos(int *num, float *x, float *y){
@@ -75,7 +193,6 @@ void receberTermos(int *num, float *x, float *y){
     printf("y[%d] = ",contador);
 		scanf("%f",y+contador);
 	}
-
 }
 
 void recebeQtdNumerosTabela(int *num){
